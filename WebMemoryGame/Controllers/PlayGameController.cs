13ea9e25c908c397e18db27dba904ecdc8cb9f6d
Vladimir Milivoyevich <milivoyevich@hotmail.com>
2018-04-03 18:36:37 +0200

    20180403-1836

--- WebMemoryGame/Controllers/PlayGameController.cs
+++ WebMemoryGame/Controllers/PlayGameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMemoryGame.Models;
 
 namespace WebMemoryGame.Controllers
 {
@@ -10,6 +11,12 @@ namespace WebMemoryGame.Controllers
     {
         // GET: PlayGame
         public ActionResult Index()
+        {
+            ViewBag.Naslov = "Memory Game";
+            return View();
+        }
+
+        public ActionResult Register(mem_korisnici MK)
         {
 
             return View();
