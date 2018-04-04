98589ad2bae83b1219ff8f5b7e122176fe2c8025
Vladimir Milivoyevich <milivoyevich@hotmail.com>
2018-04-04 16:09:06 +0200

    20180404-1547

---
+++ WebMemoryGame/App_Start/BoundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WebMemoryGame
+{
+    public class BoundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+        }
+    }
+}
--- WebMemoryGame/Controllers/PlayGameController.cs
+++ WebMemoryGame/Controllers/PlayGameController.cs
@@ -13,12 +13,35 @@ namespace WebMemoryGame.Controllers
         public ActionResult Index()
         {
             ViewBag.Naslov = "Memory Game";
+            ViewBag.Nivo = 4;
+            if(Session["Login"]!=null && Convert.ToBoolean(Session["Login"]))
+            {
+                ViewBag.Login = true;
+            }
+            else
+            {
+                ViewBag.Login = false;
+            }
             return View();
         }
 
         public ActionResult Register(mem_korisnici MK)
         {
-
+           
+            if (MK.email!=null)
+            {
+                var errorNum = ModelState.Count;
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+        public ActionResult Login(mem_korisnici MK)
+        {
+            if (MK.username != null)
+            {
+                Session["Login"] = true;
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
